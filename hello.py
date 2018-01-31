217825a3e0608dc02107ab16b3844a07c3b36f39
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-01-31 22:37:26 +0700

    Initialize database connection

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
+import os
+import sqlite3
 from flask import Flask, redirect, url_for, request, render_template
+
+# Create the application instance
 app = Flask(__name__)
+# Load config from this file
+app.config.from_object(__name__)
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'data.db'),
+    SECRET_KEY='NotForProduction',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route('/')
 def index():
